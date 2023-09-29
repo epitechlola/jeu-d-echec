9d9d1c9afe45fcde0de1e8ac4b327496f805d14c
epitechlola <144674552+epitechlola@users.noreply.github.com>
2023-09-29 14:13:15 +0200

    Update importation_images.py

--- importation_images.py
+++ importation_images.py
@@ -1,22 +1,22 @@
 import pygame
 
-echiquier = pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/echiquier.jpg")
+echiquier = pygame.image.load("./echiquier.jpg")
 
-pion_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white pawn.png")
-tour_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white rook.png")
-fou_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white bishop.png")
-cavalier_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white knigth.png")
-reine_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white queen.png")
-roi_blanc= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/white king.png")
+pion_blanc= pygame.image.load("./image piece/white pawn.png")
+tour_blanc= pygame.image.load("./image piece/white rook.png")
+fou_blanc= pygame.image.load(".image piece/white bishop.png")
+cavalier_blanc= pygame.image.load("./image piece/white knigth.png")
+reine_blanc= pygame.image.load("./image piece/white queen.png")
+roi_blanc= pygame.image.load("./image piece/white king.png")
 
-pion_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black pawn.png")
-tour_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black rook.png")
-fou_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black bishop.png")
-cavalier_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black knigth.png")
-reine_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black queen.png")
-roi_noir= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/black king.png")
+pion_noir= pygame.image.load("./image piece/black pawn.png")
+tour_noir= pygame.image.load("./image piece/black rook.png")
+fou_noir= pygame.image.load("./image piece/black bishop.png")
+cavalier_noir= pygame.image.load("./image piece/black knigth.png")
+reine_noir= pygame.image.load("./image piece/black queen.png")
+roi_noir= pygame.image.load("./image piece/black king.png")
 
-rond= pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/rond.png")
+rond= pygame.image.load("./rond.png")
 
-promotion_b=pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/promotion b.png")
-promotion_n=pygame.image.load("C:/Users/lolae/OneDrive/Bureau/python/jeu d_echec/image piece/promotion n.png")
+promotion_b=pygame.image.load("./image piece/promotion b.png")
+promotion_n=pygame.image.load("./image piece/promotion n.png")
